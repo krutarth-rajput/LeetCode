ae47b971cec03580e1bb5dd49b02a7692ae1559e
krutarth-rajput <97590416+krutarth-rajput@users.noreply.github.com>
2024-09-03 13:08:20 +0530

    Update Day57.java

--- Day57-Find the Student that Will Replace the Chalk(1894)/Day57.java
+++ Day57-Find the Student that Will Replace the Chalk(1894)/Day57.java
@@ -1,25 +1,20 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
         int n=chalk.length;
-        long[] prefix=new long[n];
-        prefix[0]=chalk[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+chalk[i];
-        }
-        System.out.print(prefix[n-1]);
-        while(k>=prefix[n-1]){
-            k=k-(int)prefix[n-1];
-        }
-        
-        int idx=-1;
+        long total=0;
         for(int i=0;i<n;i++){
-            if(prefix[i]==k){
-                idx=i+1;
-            }else if(prefix[i]>k){
-                idx=i;
-                break;
+            total=chalk[i]+total;
+        }
+        k=(int)(k%total);
+        System.out.print(k);
+        for(int i=0;i<n;i++){
+            if(chalk[i]<=k){
+                k=k-chalk[i];
+            }else{
+                return i;
+                
             }
         }
-        return idx;
+        return 1;
     }
 }
